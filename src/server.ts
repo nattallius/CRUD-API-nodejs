75190ac0afda9ee27fd90ea9422be057a041cdd9
Natallia <nattallius@gmail.com>
2024-02-13 01:05:07 +0100

    fix: fix production build

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,4 @@ import 'dotenv/config'
 const port = process.env.PORT || 3000;
 const server =  http.createServer(requestHandler);
 server.listen(+port);
+console.log(`Server listening on port ${port}`);
